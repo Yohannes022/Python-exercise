f91b4fd9eff08cff4dfaa0f0671f00ff1ac3f7b7
Yohannes022 <johannest022@gmail.com>
2026-07-27 16:53:46 +0300

    List Comprehension Master

---
+++ intermediate.py
@@ -0,0 +1,6 @@
+# Intermediate Python Exercises
+# Exercise 1: List Comprehension Mastery
+
+# words = ["apple", "bat", "cherry", "dog", "elderberry"]
+# filtered_words = [word.upper() for word in words if len(word) > 4]
+# print(filtered_words)
